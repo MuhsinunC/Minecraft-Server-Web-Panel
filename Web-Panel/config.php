0ff15347cdfc7234cbc85ce33f5a8ef0457bd3e3
Muhsinun Chowdhury <muhsinun08@gmail.com>
2017-01-22 23:13:23 -0500

    work some more

--- Web-Panel/config.php
+++ Web-Panel/config.php
@@ -16,11 +16,14 @@ return (object) array(
         "#"
     ),
 	
-	'server_ip' => '74.199.12.219',
+	'websend_ip' => '74.199.12.219',
+	'websend_port' => '4446',
 	'websend_password' => 'testpassword', //You still have to manually enter ur password
 										  //in /res/websend/Websend.php on line 10; this
 										  //is only for the other normal pages like index.php
-	
+	'ftp_username' => 'MyFTPUsername',
+	'ftp_password' => 'MyFTPPassword',
+	'ftp_ip' => '74.199.12.219',
 );
 
 ?>
--- Web-Panel/index.php
+++ Web-Panel/index.php
@@ -11,7 +11,7 @@
     include_once './res/websend/Websend.php';
 
     //Replace with bukkit server IP. To use a different port, change the constructor to: $ws = new Websend("ip", port);
-	$ws = new Websend($configs->server_ip, 4446);
+	$ws = new Websend($configs->websend_ip, $configs->websend_port);
 
     //Replace with password specified in Websend config file & also replace the password in /res/websend/Websend.php
     $ws->connect($configs->websend_password);
@@ -21,11 +21,21 @@
 	
 ?>
 
-<?php
+<div id="log">
+	<?php
+		$data = file_get_contents('ftp://'. $configs->ftp_username .':'. $configs->ftp_password .'@'. $configs->ftp_ip .'/logs/latest.log');
+	?>
 	
-	$data = file_get_contents('ftp://MyFTPUsername:MyFTPPassword@'. $configs->server_ip .'/logs/latest.log');
-
-?>
+	<?php
+		$onlinemode = $_POST['onlineMode'];
+		
+		echo $onlinemode;
+	?>
+	
+	<pre>
+		<?php echo $data;?>
+	</pre>
+</div>
 
 <!-- Load Footer -->
 	<?php include './res/templates/footer.php';?>
